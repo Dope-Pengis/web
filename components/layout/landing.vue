ca56796afbafffd523d0dda60068e06adf91d2ed
Timo972 <44401485+Timo972@users.noreply.github.com>
2022-03-19 23:53:20 +0100

    fix

--- components/layout/landing.vue
+++ components/layout/landing.vue
@@ -28,21 +28,13 @@
         with all 1111!
       </h2>
       <div class="flex flex-row gap-x-5 mt-10">
-        <DopeButton
-          custom
-          to="https://discord.dopepengis.com"
-          class="animate-float"
-        >
+        <DopeButton custom to="https://discord.dopepengis.com">
           Join Discord
           <Icon>
             <DirectLink />
           </Icon>
         </DopeButton>
-        <DopeButton
-          secondary
-          to="https://whitepaper.dopepengis.com"
-          class="animate-[floating_ease-out_18s_infinite_alternate]"
-        >
+        <DopeButton secondary to="https://whitepaper.dopepengis.com">
           Whitepaper
           <Icon>
             <DirectLink />
